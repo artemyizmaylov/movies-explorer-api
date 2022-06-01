ae9cd9b0747e6b38e1b82c8eed7d30d08245634b
Artemy Izmaylov <artemyizmaylov@gmail.com>
2022-06-01 22:51:25 +0300

    Аутентификация теперь на всех маршрутах

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,17 @@ const NotFoundError = require('../errors/NotFoundError');
 const { signup, signin, signout } = require('../controllers/users');
 const { signupPattern, signinPattern } = require('../middlewares/validation');
 const { PATH_NOT_FOUND_MSG, GREETINGS_MSG } = require('../utils/constants');
+const auth = require('../middlewares/auth');
 
 router.get('/', (req, res) => res.send({ message: GREETINGS_MSG }));
-
 router.post('/signup', signupPattern, signup);
 router.post('/signin', signinPattern, signin);
-router.post('/signout', signout);
+
+router.use(auth);
 
 router.use('/users', users);
 router.use('/movies', movies);
+router.post('/signout', signout);
 
 router.use('*', () => {
   throw new NotFoundError(PATH_NOT_FOUND_MSG);
